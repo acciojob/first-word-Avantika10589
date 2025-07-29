760d066da5c1ebf7bc7c6be84317de99a1795410
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-07-29 09:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function firstWord(s) {
   // your code here
-}
+  s = s.trimStart();
+  const spaceIndex = s.indexOf(' ');
 
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  return s.substring(0, spaceIndex);
+}
 // Do not change the code below
 
 const s = prompt("Enter String:");
